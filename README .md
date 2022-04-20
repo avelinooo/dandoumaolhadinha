20622bbd53b73f57588d37862de868cc78a80262
Avelino Ferreira de Alencar <zavelinox@Avelinos-MacBook-Air.local>
2022-04-20 16:11:23 -0300

    Mudança no texto e criação do README

---
+++ README .md
@@ -0,0 +1,2 @@
+//codigo criado com objetivo de testar atalhos git\\
+/ escrito no VSCode e upado pelo Terminal /
--- eaigalera .js
+++ eaigalera .js
@@ -1,5 +1,1 @@
-//codigo criado com objetivo de testar atalhos git - clone ; pull; push\\
-
 console.log("e ai galerinha");
-
-/ escrito no VSCode e upado pelo Terminal /
